3338c64017692b8fdafb29754698aa80ded26646
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-07-17 14:58:51 +0200

    cookies with jwt + validate request middleware

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,6 +12,12 @@ import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(json());
 
 app.use(currentUserRouter);
@@ -25,6 +32,9 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDb');
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,19 @@
-import express from 'express';
-
+import express, { NextFunction, Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validate-request';
 const router = express();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('log from signin route');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('you must supply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signinRouter };
