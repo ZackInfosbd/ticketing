582877913abfd15f40e3cb845864762f89b8df3c
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-07-18 21:04:08 +0200

    SSR getInitialProps with cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
-const index = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  // axios.get('/api/users/currentuser').catch((err) => console.log(err));
+
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // to'http://ingress-nginx-controller.ingress-nginx/api/users/currentuser'
+    const { data } = await axios.get(
+      `http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser`,
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get(`/api/users/currentuser`);
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
